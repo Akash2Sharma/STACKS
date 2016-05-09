84671399a2225bde45653dd18e4d1c45560550b1
Akash2Sharma <theakash@live.com>
2016-05-09 11:54:35 +0530

    STORY QUEUE UPLOADED

---
+++ queue.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+#include "queue.h"
+
+/*****************************************
+ *
+ * Queue
+ *
+ *****************************************/
+
+/* Story 8
+ * As a programmer, I want to be able to print a queue, so I can see
+ * what is in it.
+*/
+
+void pq(q_node q)
+{  if(q==NULL)
+	printf("queue is empty!\n");
+   else
+	while(q!=NULL){
+	printf ("%c  ", q->value);
+	q=q->next;
+	}
+}
+
+void print_queue(char *msg, queue q)
+{
+  printf("%s front: ", msg);
+  fflush(stdout);
+  pq(q.front);
+  printf(":rear\n");
+  fflush(stdout);
+}
+
+void print_queue_plus(char *before, queue q, char *after)
+{
+  printf("%s front: ", before);
+  fflush(stdout);
+  pq(q.front);
+  printf(":rear %s", after);
+  fflush(stdout);
+}
+
+/* Story 9
+ * As a programmer, I want to be able to enqueue a character in a
+ * queue, so I can learn how queues work.
+*/
+
+queue enqueue(queue q, char val)
+{ q_node a=(q_node)malloc(sizeof(q_node));
+  	a->value=val;
+	//a->next=NULL;
+	if(q.rear==NULL){
+		q.rear=a;
+		q.front=a;
+	}
+else
+	{ 
+	q.rear->next=a;
+	q.rear=a;
+	}
+q.rear->next=NULL;
+return q;
+}
+
+/* Story 10
+ * As a programmer, I want to dequeue an item from a queue so I can
+ * learn to use queues.
+ */
+
+char dequeue(queue *q)
+{	q_node a;
+	char b;
+	if((*q).front==NULL) printf("QUEUE UNDERFLOW");
+	else
+	{
+	//q_node a;
+	//char b;
+	a=(*q).front;
+	(*q).front=(*q).front->next;
+	b=a->value;
+	a->next=NULL;
+	free(a);
+	}
+return b;
+}
---
+++ queue.h
@@ -0,0 +1,36 @@
+#ifndef queue_h
+#define queue_h
+
+#define TRUE 1
+#define FALSE 0
+
+
+/* struct node is defined in stack.h
+struct node {
+  char value;
+  struct node *next;
+};*/
+
+
+typedef struct node *q_node;
+
+typedef struct queue_struct {
+  q_node front;
+  q_node rear;
+} queue;
+
+/*
+  Queues
+*/
+
+/* Story 8 */
+void print_queue(char *msg, queue q);
+void print_queue_plus(char *before, queue q, char *after);
+
+/* Story 9 */
+queue enqueue(queue q, char val);
+
+/* Story 4 */
+char dequeue(queue *q);
+
+#endif
