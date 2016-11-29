f7658af3c498e6c0e9f7ca6e6b0088f554944f25
JIHADI <theakash@live.com>
2016-11-29 18:33:57 +0530

    UPDATE

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   /* See ui.h, ui.c for initialize and execute */
   if (initialize(argc, argv)) {
     return execute();
-  }
+  } 
   return 1; /* failed to initialize */
 }
  
