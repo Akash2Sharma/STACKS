f4a0f8154c92020844a0497e7de1b214fc8eb258
JIHADI <theakash@live.com>
2017-01-15 19:24:57 +0530

    Update ui.c

--- ui.c
+++ ui.c
@@ -1,8 +1,7 @@
  #include <stdio.h>
 #include <stdlib.h>
 #include "stack.h"
-#include "ui.h" 
-
+#include "ui.h"  
 /*****************************************
  * Initialize
  * This section contains the definitions of the parameters and
